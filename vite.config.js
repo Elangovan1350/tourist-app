789ba3869e6515db01903388ec7c75d10a8bef5b
Elangovan1350 <elangovan2019miss@gmail.com>
2025-08-24 13:00:25 +0530

    change3

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
+import { resolve } from "path";
+
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"), // Your main index.html
+        about: resolve(__dirname, "src/page2.html"), // Example: your about page
+        // Add more entries for other HTML files as needed
+      },
+    },
+  },
 });
